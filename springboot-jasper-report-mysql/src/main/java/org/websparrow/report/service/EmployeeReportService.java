e5eafd9f1b7155807be7030b85ddbc780d44f790
Suren Antony Vithanage <suren_v@epiclanka.net>
2019-12-19 08:41:10 +0530

    Updated

--- springboot-jasper-report-mysql/src/main/java/org/websparrow/report/service/EmployeeReportService.java
+++ springboot-jasper-report-mysql/src/main/java/org/websparrow/report/service/EmployeeReportService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 import org.websparrow.report.entity.Employee;
 import org.websparrow.report.repository.ReportRepository;
@@ -22,16 +23,20 @@ public class EmployeeReportService {
 	@Autowired
 	private ReportRepository reportRepository;
 
+	@Autowired
+	private ResourceLoader resourceLoader;
+
 	public String generateReport() {
 		try {
 
 			List<Employee> employees = reportRepository.findAll();
 
-			String reportPath = "F:\\Content\\Report";
+//			String reportPath = "F:\\Content\\Report";
+			String reportPath = resourceLoader.getResource("classpath:employee-rpt-database.jrxml").getURI().getPath();
 
 			// Compile the Jasper report from .jrxml to .japser
-			JasperReport jasperReport = JasperCompileManager
-					.compileReport(reportPath + "\\employee-rpt-database.jrxml");
+			JasperReport jasperReport = JasperCompileManager.compileReport(reportPath);
+//					.compileReport(reportPath + "\\employee-rpt-database.jrxml");
 
 			// Get your data source
 			JRBeanCollectionDataSource jrBeanCollectionDataSource = new JRBeanCollectionDataSource(employees);
@@ -46,11 +51,14 @@ public class EmployeeReportService {
 					jrBeanCollectionDataSource);
 
 			// Export the report to a PDF file
-			JasperExportManager.exportReportToPdfFile(jasperPrint, reportPath + "\\Emp-Rpt-Database.pdf");
+//			JasperExportManager.exportReportToPdfFile(jasperPrint, reportPath + "\\Emp-Rpt-Database.pdf");
+			JasperExportManager.exportReportToPdfFile(jasperPrint, reportPath);
 
 			System.out.println("Done");
 
-			return "Report successfully generated @path= " + reportPath;
+//			return "Report successfully generated @path= " + reportPath;
+			return "Report successfully generated";
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			return "Error--> check the console log";
